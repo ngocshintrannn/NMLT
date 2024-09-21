bb728d75cba6706b6bfddd5b71d14152bcb62480
ngocshintrannn <ngoc.shintrann@gmail.com>
2024-09-21 17:00:02 +0700

    upload full tuan 5

--- TH_tuan5.md
+++ TH_tuan5.md
@@ -725,6 +725,60 @@ int main() {
 #### Bài 7
 
 ```c
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  char player1, player2;
+
+  cout << "Nhap player 1, player 2: ";
+  cin >> player1 >> player2;
+
+  switch (player1) {
+    case 'B': case 'b':
+      switch (player2) {
+        case 'B': case 'b':
+          cout << "Hoa\n";
+          break;
+        case 'K': case 'k':
+          cout << "Player 1 thang\n";
+          break;
+        case 'G': case 'g':
+          cout << "Player 2 thang\n";
+          break;
+      }
+      break;
+    case 'K': case 'k':
+      switch (player2) {
+        case 'B': case 'b':
+          cout << "Player 2 thang\n";
+          break;
+        case 'K': case 'k':
+          cout << "Hoa\n";
+          break;
+        case 'G': case 'g':
+          cout << "Player 1 thang\n";
+          break;
+      }
+      break;
+    case 'G': case 'g':
+      switch (player2) {
+        case 'B': case 'b':
+          cout << "Player 1 thang\n";
+          break;
+        case 'K': case 'k':
+          cout << "Player 2 thang\n";
+          break;
+        case 'G': case 'g':
+          cout << "Hoa\n";
+          break;
+      }
+      break;
+  }
+
+  return 0;
+}
 ```
 
 #### Bài 8
--- compiler/main.cpp
+++ compiler/main.cpp
@@ -2,67 +2,52 @@
 
 using namespace std;
 
-bool isLeapYear(int year) {
-  return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-}
-
-bool isValidDate(int day, int month, int year) {
-  if (year < 1 || month < 1 || month > 12 || day < 1) {
-    return false;
-  }
-
-  int daysInMonth;
-  switch (month) {
-    case 4: case 6: case 9: case 11:
-      daysInMonth = 30;
-      break;
-    case 2:
-      daysInMonth = isLeapYear(year) ? 29 : 28;
-      break;
-    default:
-      daysInMonth = 31;
-  }
-
-  return day <= daysInMonth;
-}
-
 int main() {
-  int ngay, thang, nam;
-  string ngayThang;
+  char player1, player2;
 
-  cout << "Nhap ngay, thang, nam: ";
-  cin >> ngay >> thang >> nam;
+  cout << "Nhap player 1, player 2: ";
+  cin >> player1 >> player2;
 
-  if (!isValidDate(ngay, thang, nam)) {
-    cout << "Du lieu khong hop le!" << endl;
-    return 1;
-  }
-
-  switch (ngay) {
-    case 1: ngayThang = "1st"; cout << ngayThang; break;
-    case 2: ngayThang = "2nd"; cout << ngayThang; break;
-    case 3: ngayThang = "3rd"; cout << ngayThang; break;
-    case 21: ngayThang = "21st"; cout << ngayThang; break;
-    case 22: ngayThang = "22nd"; cout << ngayThang; break;
-    case 23: ngayThang = "23rd"; cout << ngayThang; break;
-    case 31: ngayThang = "31st"; cout << ngayThang; break;
-    default: ngayThang = to_string(ngay) + "th"; cout << ngayThang;
-  }
-
-  switch(thang) {
-    case 1: cout << " January " << nam << endl; break;
-    case 2: cout << " February " << nam << endl; break;
-    case 3: cout << " March " << nam << endl; break;
-    case 4: cout << " April " << nam << endl; break;
-    case 5: cout << " May " << nam << endl; break;
-    case 6: cout << " June " << nam << endl; break;
-    case 7: cout << " July " << nam << endl; break;
-    case 8: cout << " August " << nam << endl; break;
-    case 9: cout << " September " << nam << endl; break;
-    case 10: cout << " October " << nam << endl; break;
-    case 11: cout << " November " << nam << endl; break;
-    case 12: cout << " December " << nam << endl; break;
-    default: cout << "Invalid month!" << endl; break;
+  switch (player1) {
+    case 'B': case 'b':
+      switch (player2) {
+        case 'B': case 'b':
+          cout << "Hoa\n";
+          break;
+        case 'K': case 'k':
+          cout << "Player 1 thang\n";
+          break;
+        case 'G': case 'g':
+          cout << "Player 2 thang\n";
+          break;
+      }
+      break;
+    case 'K': case 'k':
+      switch (player2) {
+        case 'B': case 'b':
+          cout << "Player 2 thang\n";
+          break;
+        case 'K': case 'k':
+          cout << "Hoa\n";
+          break;
+        case 'G': case 'g':
+          cout << "Player 1 thang\n";
+          break;
+      }
+      break;
+    case 'G': case 'g':
+      switch (player2) {
+        case 'B': case 'b':
+          cout << "Player 1 thang\n";
+          break;
+        case 'K': case 'k':
+          cout << "Player 2 thang\n";
+          break;
+        case 'G': case 'g':
+          cout << "Hoa\n";
+          break;
+      }
+      break;
   }
 
   return 0;
